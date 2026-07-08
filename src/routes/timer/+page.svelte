532a1a26ad982ca81d5f9b30c1c847afaa12ece8
pkb-pmj <pkb.pmj@gmail.com>
2026-07-08 13:45:12 +0200

    ♻️ 1/3 add layout-specific code to /timer/+page.svelte

--- src/routes/timer/+page.svelte
+++ src/routes/timer/+page.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
 	import Sequence from '$lib/components/Sequence.svelte';
+
+	let { children } = $props();
 </script>
 
 <a href="/">Home</a>
 <Sequence />
+
+{@render children()}
